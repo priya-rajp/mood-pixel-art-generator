649da03c4a2086fc2a2e2556a1ff34941f36c912
Priya Rajpurohit <42963408+priya-rajp@users.noreply.github.com>
2025-06-09 11:16:52 +0100

    Update server.js

--- mood-pixel-backend/server.js
+++ mood-pixel-backend/server.js
@@ -9,12 +9,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error(err));
+//mongoose.connect(process.env.MONGO_URI, {
+//  useNewUrlParser: true,
+//  useUnifiedTopology: true
+//})
+//.then(() => console.log('Connected to MongoDB'))
+//.catch(err => console.error(err));
 
 const artRoutes = require('./routes/art');
 app.use('/api/art', artRoutes);
